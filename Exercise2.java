d83bf5cdba14977f7b0bdabf26dafab1df66df90
OBrien McQuade <62272369+McFlyin619@users.noreply.github.com>
2020-03-30 20:17:28 -0700

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,15 @@
+class Exercise2{
+    public static void main(String[] args){
+        String name;
+        int age;
+        double salary;
+
+        name="OBrien";
+        age=32;
+        salary=15000.20;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
